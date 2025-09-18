be66239dc3ef14874a905d739da5250c998351df
moncy <elmoncysercle@gmail.com>
2025-09-18 17:09:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
+import os, io, datetime, pandas as pd, yaml, json
+import streamlit as st
+
 from pathlib import Path
 ROOT = Path(__file__).resolve().parent
 if str(ROOT) not in sys.path:
